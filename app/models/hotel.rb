cc8beacd15847b561ae64fec0188560149a279e4
Giau Ngo <giaunv@gmail.com>
2015-05-23 12:29:47 +0700

    Adding Hotel Validations

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,5 @@
 class Hotel < ActiveRecord::Base
+	validates :name, presence: true, length: {minimum:5, maximum: 100}
+	validates :image, presence: true, length: {minimum:5, maximum: 100}
+	validates :address, presence: true, length: {minimum:2, maximum: 100}
 end
