cf319831574f5a5384f25ad7d2b9ed5da0199859
Giau Ngo <giaunv@gmail.com>
2015-05-23 12:33:11 +0700

    Associating Models

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,6 @@
 class Hotel < ActiveRecord::Base
+	has_many :comments
+
 	validates :name, presence: true, length: {minimum:5, maximum: 100}
 	validates :image, presence: true, length: {minimum:5, maximum: 100}
 	validates :address, presence: true, length: {minimum:2, maximum: 100}
